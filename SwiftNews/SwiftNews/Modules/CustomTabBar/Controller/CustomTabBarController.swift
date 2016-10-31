3d9ee2d252118d6c668cdd6a83766b19c5b0ca28
Mike Mikina <mike@mikina.pl>
2016-10-31 23:04:19 +0100

    Empty custom tab bar controller and directory structure.

---
+++ SwiftNews/SwiftNews/Modules/CustomTabBar/Controller/CustomTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTabBarController.swift
+//  SwiftNews
+//
+//  Created by Mike Mikina on 10/31/16.
+//  Copyright © 2016 SwiftCookies.com. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
